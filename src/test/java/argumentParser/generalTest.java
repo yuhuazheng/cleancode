db0c0478d55249f15c8dae49c2a1e926bd1582eb
yuhuazheng <yuhuazheng2006@gmail.com>
2017-02-19 23:17:17 -0800

    initials and first test mock

---
+++ src/test/java/argumentParser/generalTest.java
@@ -0,0 +1,20 @@
+package argumentParser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yuhuazheng on 2/19/17.
+ */
+public class generalTest {
+    @Test
+    public void testBoolean() throws Exception {
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void testString() throws Exception {
+        assertEquals(true, true);
+    }
+}
