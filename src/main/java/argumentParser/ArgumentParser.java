e12a19ea2e0dd10dd099a8ab148353f138e0be96
yuhuazheng <yuhuazheng2006@gmail.com>
2017-02-20 15:27:29 -0800

    update of boolean test

--- src/main/java/argumentParser/ArgumentParser.java
+++ src/main/java/argumentParser/ArgumentParser.java
@@ -1,19 +1,118 @@
 package argumentParser;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by yuhuazheng on 2/19/17.
  */
 public class ArgumentParser {
 
-    public ArgumentParser(String schema, String[] args){
+    private String schema;
+    private String[] args;
+    private Set<Character> unexpectedArgs = new TreeSet<Character>();
+    private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
 
+    private boolean valid;
+    private int numOfArgs = 0;
+
+    public ArgumentParser(String schema, String[] args){
+        this.schema = schema;
+        this.args = args;
+        valid = parse();
+    }
+
+    public boolean isValid(){
+        return valid;
+    }
+
+    public int cardinality(){
+        return numOfArgs;
+    }
+
+    private boolean parse(){
+        if(schema.length()==0 && args.length == 0)
+            return true;
+        parseSchema();
+        parseArgs();
+        return unexpectedArgs.size() == 0;
+    }
+
+    private void parseSchema(){
+        for(String schemaElement : schema.split(",")){
+            parseSchemaElement(schemaElement);
+        }
+    }
+
+    private void parseSchemaElement(String schemaElement){
+        if(schemaElement.length() == 1){
+            parseBooleanSchemaElement(schemaElement);
+        }
+    }
+
+    private void parseBooleanSchemaElement(String schemaElement){
+        char c = schemaElement.charAt(0);
+        if(Character.isLetter(c))
+            setBooleanArgument(c, false);
+    }
+
+    private void setBooleanArgument(Character c, boolean b){
+        booleanArgs.put(c, b);
+    }
+
+    private void parseArgs(){
+        for(String arg : args){
+            parseArg(arg);
+        }
+    }
+
+    private void parseArg(String arg){
+        if(arg.startsWith("-"))
+            parseArgElements(arg.substring(1));
+    }
+
+    private void parseArgElements(String argElements){
+        for(Character c : argElements.toCharArray()){
+            parseArgElement(c);
+        }
+    }
+
+    private void parseArgElement(Character c){
+        if(isBooleanSchema(c)){
+            numOfArgs++;
+            setBooleanArgument(c, true);
+        }
+        else{
+            unexpectedArgs.add(c);
+        }
+    }
+
+    private boolean isBooleanSchema(Character c){
+        return booleanArgs.containsKey(c);
     }
 
     public boolean has(Character c){
-        return true;
+        return booleanArgs.containsKey(c);
     }
 
     public boolean getBoolean(Character c){
-        return true;
+        return booleanArgs.get(c);
+    }
+
+    public String errorMessage() {
+        if(unexpectedArgs.size() > 0)
+            return unexpectedArgsMessage();
+        else
+            return "";
+    }
+
+    private String unexpectedArgsMessage(){
+        StringBuffer sb = new StringBuffer("unexpected arguments: ");
+        for(Character c : unexpectedArgs){
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
--- src/test/java/argumentParser/generalTest.java
+++ src/test/java/argumentParser/generalTest.java
@@ -9,12 +9,22 @@ import static org.junit.Assert.assertEquals;
  */
 public class generalTest {
     @Test
-    public void testBoolean() throws Exception {
+    public void testBooleanSchema() throws Exception {
         String schema = "b";
         String[] args = {"-b"};
         ArgumentParser argumentParser =  new ArgumentParser(schema, args);
         assertEquals(true, argumentParser.has('b'));
         assertEquals(true, argumentParser.getBoolean('b'));
+    }
+
+    @Test
+    public void testUnexpectedBoolean() throws Exception {
+        String schema = "b";
+        String[] args = {"-c"};
+        ArgumentParser argumentParser =  new ArgumentParser(schema, args);
+        assertEquals(true, argumentParser.has('b'));
+        assertEquals(false, argumentParser.getBoolean('b'));
+        assertEquals(true, argumentParser.errorMessage().contains("c"));
     }
 
     @Test
